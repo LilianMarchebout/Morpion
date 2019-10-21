20fe864e205f43931d54310438ef564a41f669fc
Lilian <35975697+Nalil10@users.noreply.github.com>
2019-10-21 11:33:46 +0200

    Update Interpretor.py

---
+++ Interpretor.py
@@ -0,0 +1,12 @@
+
+from tkinter import Button, Tk
+fenetre = Tk()
+def fonc(name):
+    fonction(name)
+bouton = Button(fenetre, text="HELLO WORLD!", command=fonc(5))
+def fonction(name):
+    texte = 'hello world!' + str(name)
+    bouton.config(text=texte)
+bouton.pack()
+fenetre.mainloop()
+
