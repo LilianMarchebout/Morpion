87c86f5f1eac832b7d8f759c1ea582bcc3c953b0
Lilian <35975697+Nalil10@users.noreply.github.com>
2019-10-21 11:33:48 +0200

    Update Morpion.py

--- Morpion.py
+++ Morpion.py
@@ -26,42 +26,38 @@ from time import sleep #Permet de faire attendre le programme notamment pour met
 
 
 
+##VARIABLE
+#Dictionnaire avec comme clef le nom de la case, et comme valeur une liste avec l'appartenance à un joueur, le nombre de points, l'abscisse, l'ordonnée, le numéro de la case
+case ={str(chr(c)+str(n)):[False, 0, (n-1)*240, ("ZABC".index(chr(c))-1)*240, int(("A0A1A2A3B1B2B3C1C2C3".index(chr(c)+str(n)))/2)] for c in range(65, 68) for n in range(1, 4)}
+"""
+{'A1': [False, 0, 0, 0, 1],
+'A2': [False, 0, 240, 0, 2], 
+'A3': [False, 0, 480, 0, 3], 
+'B1': [False, 0, 0, 240, 4], 
+'B2': [False, 0, 240, 240, 5], 
+'B3': [False, 0, 480, 240, 6], 
+'C1': [False, 0, 0, 480, 7], 
+'C2': [False, 0, 240, 480, 8], 
+'C3': [False, 0, 480, 480, 9]}
+"""
+
+
+#Variable permettant de déterminer quel joueur joue 
+player = 0
+
+
+#Liste pour les boutons
+buttons = []
+
+
+
+
 ##FONCTION
-
-def grid():
-    """
-    Crée la grille avec l'interface tkinter et crée les boutons
-    """
-    
-    f = Tk() #Ouverture de l'interface et définition de la fenêtre
-    f.geometry("720x720") #Dimension de l'interface
-    default = PhotoImage(file='./Image/default.png')
-    
-    #Boutons:
-    #Le x=0 est le bord gauche de la fenetre
-    #Le y=0 est le bord haut de la fenetre
-    #La position est la position du coin en haut à gauche du bouton
-
-    # Création des boutons
-    global inter
-    inter = 0
-    for nameCase in case.keys():
-        newbutton = Button(f, image = default, text=nameCase, height = 240, width = 240) #Définition du bouton
-        buttons.append(newbutton) #Ajouter à la liste
-        newbutton.config(command = intermediate(nameCase)) #Commande du bouton
-        substitute = case[nameCase] 
-        newbutton.place(x= substitute[2], y= substitute[3]) #Placement du bouton
-    f.mainloop() #Fermeture de l'interface
-    inter = 1
-    global player
-    player = 0
-
-
-
 def intermediate(name):
     """
     Sert à que bouton ne lance pas la commande lors de sa définition.
     """
+    global player
     global inter 
     print(inter)
     if inter == 1:
@@ -108,6 +104,39 @@ def game(name):
 
 
 
+def grid():
+    """
+    Crée la grille avec l'interface tkinter et crée les boutons
+    """
+    
+    f = Tk() #Ouverture de l'interface et définition de la fenêtre
+    f.geometry("720x720") #Dimension de l'interface
+    default = PhotoImage(file='./Image/default.png')
+
+
+
+    #Boutons:
+    #Le x=0 est le bord gauche de la fenetre
+    #Le y=0 est le bord haut de la fenetre
+    #La position est la position du coin en haut à gauche du bouton
+
+    # Création des boutons
+    global inter
+    global player
+    inter = 0
+    for nameCase in case.keys():
+        newbutton = Button(f, image = default, text=nameCase, height = 240, width = 240) #Définition du bouton
+        buttons.append(newbutton) #Ajouter à la liste
+        newbutton.config(command = intermediate(nameCase)) #Commande du bouton
+        substitute = case[nameCase] 
+        newbutton.place(x= substitute[2], y= substitute[3]) #Placement du bouton
+    f.mainloop() #Fermeture de l'interface
+    inter = 1
+    global player
+    player = 0
+
+
+
 def files():
     """
     Fichier pour inscrire les éléments du dictionnaire
@@ -119,31 +148,6 @@ def files():
     for nameCase in case.keys():
         folder.write(nameCase + " = " +str(case[nameCase]) + "\n")
     folder.close()
-
-
-
-##VARIABLE
-#Dictionnaire avec comme clef le nom de la case, et comme valeur une liste avec l'appartenance à un joueur, le nombre de points, l'abscisse, l'ordonnée, le numéro de la case
-case ={str(chr(c)+str(n)):[False, 0, (n-1)*240, ("ZABC".index(chr(c))-1)*240, int(("A0A1A2A3B1B2B3C1C2C3".index(chr(c)+str(n)))/2)] for c in range(65, 68) for n in range(1, 4)}
-"""
-{'A1': [False, 0, 0, 0, 1],
-'A2': [False, 0, 240, 0, 2], 
-'A3': [False, 0, 480, 0, 3], 
-'B1': [False, 0, 0, 240, 4], 
-'B2': [False, 0, 240, 240, 5], 
-'B3': [False, 0, 480, 240, 6], 
-'C1': [False, 0, 0, 480, 7], 
-'C2': [False, 0, 240, 480, 8], 
-'C3': [False, 0, 480, 480, 9]}
-"""
-files()
-
-#Variable permettant de déterminer quel joueur joue 
-player = 0
-
-
-#Liste pour les boutons
-buttons = []
 
 
 
