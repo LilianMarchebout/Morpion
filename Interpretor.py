de670078ffa0c0a1527ce2f5dbd7986d6fbb173b
Lilian <35975697+Nalil10@users.noreply.github.com>
2019-10-23 14:12:33 +0200

    Update Interpretor.py

--- Interpretor.py
+++ Interpretor.py
@@ -4,10 +4,12 @@ File allowing to test lines codes. Will be deleted after.
 
 from tkinter import Button, Tk, PhotoImage
 fenetre = Tk()
-image = PhotoImage(file="./Image/cercle.png")
-bouton = Button(fenetre, command=photo("a1"))
-bouton.pack()
-def photo(name):
+def photo():
     global bouton
-    bouton.config(image=image, name= name)
+    global name
+    bouton.config(image=image, text=name)
+image = PhotoImage(file="./Image/cercle.png")
+name = "a1"
+bouton = Button(fenetre, command=photo)
+bouton.pack()
 fenetre.mainloop()
