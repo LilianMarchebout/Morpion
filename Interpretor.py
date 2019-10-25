c59d1c900f996451bca4c353373fdc9fd26da902
Nalil10 <liliannalil347@gmail.com>
2019-10-25 22:30:04 +0200

    Update Interpretor.py

--- Interpretor.py
+++ Interpretor.py
@@ -8,7 +8,7 @@ def photo():
     global bouton
     global name
     bouton.config(image=image, text=name)
-image = PhotoImage(file="./Image/cercle.png")
+image = PhotoImage(file="./Image/croix.png")
 name = "a1"
 bouton = Button(fenetre, command=photo)
 bouton.pack()
